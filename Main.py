4648aaf1622fd8eb8600df9165921e0b02813387
Janko Varga <varga.janko@yahoo.de>
2019-10-27 22:45:58 +0100

    done simulation

--- Main.py
+++ Main.py
@@ -4,31 +4,36 @@ from typing import List, Any
 
 
 
-heap = []  #Zeitpunkt, Prio, Nr, Funktion
+heap: List[Any] = []  #Zeitpunkt, Prio, Nr, Funktion
 counter = itertools.count()
 
 
 class Station:
+
     def __init__(self, Dauer, Liste, bEmpty = 0):
-        self.Warteschlange = Liste
+        self.Warteschlange: List[Any] = Liste
         self.bEmpty = bEmpty
-        self.Dauer = Dauer
+        self.Dauer: int = Dauer
 
     def isEmpty(self):
         return self.bEmpty
 
     def add(self, Kunde):
-        self.Warteschlange.append(Kunde)
         self.bEmpty = 1;
+        self.Warteschlange.append(Kunde.Name)
 
-    def entry(self):
-        return 0
+
+    def isMyTurn(self, Kundename):
+        if(self.Warteschlange[0] == Kundename):
+            return True
+        return False
 
     def remove(self, Kunde):
-        if(len(self.Warteschlange) == 1):
+        if(len(self.Warteschlange) == 1 and self.Warteschlange.count(Kunde.Name) > 0):
             self.bEmpty = 0
-
-        return self.Warteschlange.remove(Kunde)
+            self.Warteschlange.remove(Kunde.Name)
+        elif(len(self.Warteschlange) > 1 and self.Warteschlange.count(Kunde.Name) > 0):
+            self.Warteschlange.remove(Kunde.Name)
 
 Theken = []
 # Theke[0] = Baecker, [1] = kaese, [2] = Wurst, [3] = kasse
@@ -41,10 +46,10 @@ Theken.append(Station(5, []))
 class Customer:
     def __init__(self, Kundeninformation, Name = ""):
         self.Name = Name
-        self.Position = 0
+        self.Position = -1
         self.Laufen = Kundeninformation[0]
         self.WarteschlangeMax = Kundeninformation[1]
-        self.KaufeAnzahl = Kundeninformation[2]
+        self.KaufeAnzahl: int = Kundeninformation[2]
 
     def anzahlDerEinkaeufe(self):
         return len(self.Laufen)
@@ -100,17 +105,82 @@ class Supermarkt:
         CustomerT1_5 = Customer(self.KundeninfoTypEins, "T1_5")
         CustomerT2_5 = Customer(self.KundeninfoTypZwei, "T2_5")
 
+        CustomerT1_6 = Customer(self.KundeninfoTypEins, "T1_6")
+        CustomerT2_6 = Customer(self.KundeninfoTypZwei, "T2_6")
+        CustomerT1_7 = Customer(self.KundeninfoTypEins, "T1_7")
+        CustomerT2_7 = Customer(self.KundeninfoTypZwei, "T2_7")
+        CustomerT1_8 = Customer(self.KundeninfoTypEins, "T1_8")
+        CustomerT2_8 = Customer(self.KundeninfoTypZwei, "T2_8")
+        CustomerT1_9 = Customer(self.KundeninfoTypEins, "T1_9")
+        CustomerT2_9 = Customer(self.KundeninfoTypZwei, "T2_9")
+        CustomerT1_10 = Customer(self.KundeninfoTypEins, "T1_10")
+        CustomerT2_10 = Customer(self.KundeninfoTypZwei, "T2_10")
+
+        CustomerT2_11 = Customer(self.KundeninfoTypZwei, "T2_11")
+        CustomerT2_12 = Customer(self.KundeninfoTypZwei, "T2_12")
+        CustomerT2_13 = Customer(self.KundeninfoTypZwei, "T2_13")
+        CustomerT2_14 = Customer(self.KundeninfoTypZwei, "T2_14")
+        CustomerT2_15 = Customer(self.KundeninfoTypZwei, "T2_15")
+        CustomerT2_16 = Customer(self.KundeninfoTypZwei, "T2_16")
+        CustomerT2_17 = Customer(self.KundeninfoTypZwei, "T2_17")
+        CustomerT2_18 = Customer(self.KundeninfoTypZwei, "T2_18")
+        CustomerT2_19 = Customer(self.KundeninfoTypZwei, "T2_19")
+        CustomerT2_20 = Customer(self.KundeninfoTypZwei, "T2_20")
+
+        CustomerT2_21 = Customer(self.KundeninfoTypZwei, "T2_21")
+        CustomerT2_22 = Customer(self.KundeninfoTypZwei, "T2_22")
+        CustomerT2_23 = Customer(self.KundeninfoTypZwei, "T2_23")
+        CustomerT2_24 = Customer(self.KundeninfoTypZwei, "T2_24")
+        CustomerT2_25 = Customer(self.KundeninfoTypZwei, "T2_25")
+        CustomerT2_26 = Customer(self.KundeninfoTypZwei, "T2_26")
+        CustomerT2_27 = Customer(self.KundeninfoTypZwei, "T2_27")
+        CustomerT2_28 = Customer(self.KundeninfoTypZwei, "T2_28")
+        CustomerT2_29 = Customer(self.KundeninfoTypZwei, "T2_29")
+        CustomerT2_30 = Customer(self.KundeninfoTypZwei, "T2_30")
+
         #alle Kunden auf den heap setzen.
         add_task(0, CustomerT1_1, walk, [0, 2, 1, 3])
-        add_task(200, CustomerT1_2, walk, [0, 1, 2, 3])
-        add_task(400, CustomerT1_3, walk, [0, 1, 2, 3])
-        add_task(600, CustomerT1_4, walk, [0, 1, 2, 3])
-        add_task(800, CustomerT1_5, walk, [0, 1, 2, 3])
+        add_task(200, CustomerT1_2, walk, [0, 2, 1, 3])
+        add_task(400, CustomerT1_3, walk, [0, 2, 1, 3])
+        add_task(600, CustomerT1_4, walk, [0, 2, 1, 3])
+        add_task(800, CustomerT1_5, walk, [0, 2, 1, 3])
+        add_task(1000, CustomerT1_6, walk, [0, 2, 1, 3])
+        add_task(1200, CustomerT1_7, walk, [0, 2, 1, 3])
+        add_task(1400, CustomerT1_8, walk, [0, 2, 1, 3])
+        add_task(1600, CustomerT1_9, walk, [0, 2, 1, 3])
+        add_task(1800, CustomerT1_10, walk, [0, 2, 1, 3])
+
         add_task(1, CustomerT2_1, walk, [2, 3, 0])
-        add_task(61, CustomerT2_2, walk, [0, 1, 2])
-        add_task(121, CustomerT2_3, walk, [0, 1, 2])
-        add_task(181, CustomerT2_4, walk, [0, 1, 2])
-        add_task(241, CustomerT2_5, walk, [0, 1, 2])
+        add_task(61, CustomerT2_2, walk, [2, 3, 0])
+        add_task(121, CustomerT2_3, walk, [2, 3, 0])
+        add_task(181, CustomerT2_4, walk, [2, 3, 0])
+        add_task(241, CustomerT2_5, walk, [2, 3, 0])
+        add_task(301, CustomerT2_6, walk, [2, 3, 0])
+        add_task(361, CustomerT2_7, walk, [2, 3, 0])
+        add_task(421, CustomerT2_8, walk, [2, 3, 0])
+        add_task(481, CustomerT2_9, walk, [2, 3, 0])
+        add_task(541, CustomerT2_10, walk, [2, 3, 0])
+        add_task(601, CustomerT2_11, walk, [2, 3, 0])
+        add_task(661, CustomerT2_12, walk, [2, 3, 0])
+        add_task(721, CustomerT2_13, walk, [2, 3, 0])
+        add_task(781, CustomerT2_14, walk, [2, 3, 0])
+        add_task(841, CustomerT2_15, walk, [2, 3, 0])
+
+        add_task(901, CustomerT2_16, walk, [2, 3, 0])
+        add_task(961, CustomerT2_17, walk, [2, 3, 0])
+        add_task(1021, CustomerT2_18, walk, [2, 3, 0])
+        add_task(1081, CustomerT2_19, walk, [2, 3, 0])
+        add_task(1141, CustomerT2_20, walk, [2, 3, 0])
+        add_task(1201, CustomerT2_21, walk, [2, 3, 0])
+        add_task(1261, CustomerT2_22, walk, [2, 3, 0])
+        add_task(1321, CustomerT2_23, walk, [2, 3, 0])
+        add_task(1381, CustomerT2_24, walk, [2, 3, 0])
+        add_task(1441, CustomerT2_25, walk, [2, 3, 0])
+        add_task(1501, CustomerT2_26, walk, [2, 3, 0])
+        add_task(1561, CustomerT2_27, walk, [2, 3, 0])
+        add_task(1621, CustomerT2_28, walk, [2, 3, 0])
+        add_task(1681, CustomerT2_29, walk, [2, 3, 0])
+        add_task(1741, CustomerT2_30, walk, [2, 3, 0])
 
         self.EventHandler()
 
@@ -120,17 +190,19 @@ class Supermarkt:
             Zeit, count, Kunde, func, order = heappop(heap)
             #arbeite die aktion des kunden ab(laufen/kaufen/warten)
             tZeit, tfunc = func(Kunde, order)
+
             #kunde nur in die queue wieder reinsetzten wenn func nicht end ist
             if(tfunc != end):
                 #berechne wann der kunde wieder eine aktion ausführen will
                 Zeit = Zeit + tZeit
-                print(Kunde.Name ,Zeit, tfunc)
+                #print(Kunde.Name ,Zeit, tfunc)
                 #setze den kunden wieder auf die heapque
                 add_task(Zeit, Kunde, tfunc, order)
                 # "debug" pfusch am bau
                 #print(Zeit, Kunde, func, num)
             else:
                 print(Kunde.Name ,Zeit + tZeit, tZeit, tfunc)
+                end(Kunde, order)
 
 def add_task(Zeit, Kundeninfo, tfunc, order):
     count = next(counter)
@@ -144,15 +216,19 @@ def add_task(Zeit, Kundeninfo, tfunc, order):
 ##################################################
 #wenn kunde von station zu station will
 ##################################################
-def walk(Kundeninfo, order):
-    return (Kundeninfo.walk(), entry)
+def walk(Kunde, order):
+    Theken[order[Kunde.getPosition()]].remove(Kunde)
+    Kunde.incPosition()
+    return (Kunde.walk(), entry)
 
 ##################################################
 #kunde wartet an der station
 ##################################################
 def wait(Kunde, order):
-    Theken[order[Kunde.getPosition()]].add(Kunde)
-    return (len(Theken[order[Kunde.getPosition()]].Warteschlange) * Theken[order[Kunde.getPosition()]].Dauer, service)
+    if(Theken[order[Kunde.getPosition()]].isMyTurn(Kunde.Name)):
+        return service(Kunde, order)
+    else:
+        return (Theken[order[Kunde.getPosition()]].Dauer, wait)
 
 ##################################################
 #kunde kommt an der sation an
@@ -160,20 +236,22 @@ def wait(Kunde, order):
 def entry(Kunde, order):
     if(Kunde.getWarteschlangeMax() > len(Theken[order[Kunde.getPosition()]].Warteschlange)):
         if(Theken[order[Kunde.getPosition()]].bEmpty == 0):
-            #TODO: Kunden in die Warteschlange der Station setzen
+             Theken[order[Kunde.getPosition()]].add(Kunde)
              return service(Kunde, order)
         else:
-            # TODO: Kunden in die Warteschlange der Station setzen
+            Theken[order[Kunde.getPosition()]].add(Kunde)
             return wait(Kunde, order)
     else:
-        Kunde.incPosition()
+        #Kunde.incPosition()
+        print("Warteschlange voll",Kunde.getPosition())
         return walk(Kunde, order)
 
 #####################################################
 #kunde ist feritg mit den einkauf
 #####################################################
-def end():
+def end(Kunde, order):
     print("end")
+    Theken[order[Kunde.getPosition()]].remove(Kunde)
     return 0
 
 ###################################################
@@ -185,7 +263,7 @@ def service(Kunde, order):
         return Theken[order[Kunde.getPosition()]].Dauer * Kunde.KaufeAnzahl[Kunde.getPosition()], end
     else:
         tmp = Theken[order[Kunde.getPosition()]].Dauer * Kunde.KaufeAnzahl[Kunde.getPosition()]
-        Kunde.incPosition()
+        print(Kunde.Name, tmp)
         return tmp, walk
 
 
